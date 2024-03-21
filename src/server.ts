91a20c845f7813ee8cf6b8c738bfcdc73c31e787
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2024-03-21 16:33:47 +0700

    Gotting bugs value which is responsed undefined at sign-up page

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,18 @@ import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc";
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
+
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({});
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
 
 const start = async () => {
   const payload = await getPayloadClient({
@@ -17,6 +26,15 @@ const start = async () => {
   });
 
   app.use((req, res) => nextHandler(req, res));
+
+  //routes
+  app.use(
+    "/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+    })
+  );
+
   nextApp.prepare().then(() => {
     payload.logger.info(`Nextjs started`);
     app.listen(port, async () => {
---
+++ src/trpc/trpc.ts
@@ -0,0 +1,6 @@
+import { Context } from "@/server";
+import { initTRPC } from "@trpc/server";
+
+const t = initTRPC.context<Context>().create();
+export const router = t.router;
+export const publicProcedure = t.procedure;
